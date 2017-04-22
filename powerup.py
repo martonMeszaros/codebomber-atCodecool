70adb90efdbe3450cf48d7dfef4f0860beecafca
martonMeszaros <marton.m95@gmail.com>
2017-04-22 17:08:51 +0200

    Renamed misleading variable 'n_of_powerups' to 'n_of_powerup_per_type'

--- powerup.py
+++ powerup.py
@@ -15,11 +15,11 @@ class Powerup(sdl2.ext.Entity):
     power = 1
     speed = 2
 
-    n_of_powerups = 6
+    n_of_powerup_per_type = 6
     remaining_powerups = []
 
     def reset_remaining_powerups():
-        for i in range(Powerup.n_of_powerups):
+        for i in range(Powerup.n_of_powerup_per_type):
             Powerup.remaining_powerups.append(Powerup.bombcount)
             Powerup.remaining_powerups.append(Powerup.power)
             Powerup.remaining_powerups.append(Powerup.speed)
