4c0c3e1504ba046305ea02a4a7cc76df7551e5a3
martonMeszaros <marton.m95@gmail.com>
2017-04-22 14:22:42 +0200

    Rearranged vairables in common.py

--- common.py
+++ common.py
@@ -6,11 +6,12 @@ import sdl2.ext
 class Color(object):
     """Contains sdl2.ext Color objects with colors
     defined in google's material design.
+    https://material.io/guidelines/style/color.html#color-color-tool
     """
-    grass = sdl2.ext.Color(76, 175, 80)             # Green 500
-    wall_permanent = sdl2.ext.Color(117, 117, 117)  # Grey 600
-    wall = sdl2.ext.Color(189, 189, 189)            # Grey 400
     black = sdl2.ext.Color(0, 0, 0)                 # Black
+    grass = sdl2.ext.Color(76, 175, 80)             # Green 500
     powerup_power = sdl2.ext.Color(255, 197, 3)     # Amber 500
     powerup_bombcount = sdl2.ext.Color(55, 71, 79)  # Blue Grey 800
     powerup_speed = sdl2.ext.Color(3, 169, 244)     # Light Blue 500
+    wall_permanent = sdl2.ext.Color(117, 117, 117)  # Grey 600
+    wall = sdl2.ext.Color(189, 189, 189)            # Grey 400
