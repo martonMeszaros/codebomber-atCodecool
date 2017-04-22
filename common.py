e49e8355fe14e7b6e40f1d80e92b04e1f5cf8e71
martonMeszaros <marton.m95@gmail.com>
2017-04-22 16:59:48 +0200

    Changed the color of power powerup in common.py

--- common.py
+++ common.py
@@ -10,7 +10,7 @@ class Color(object):
     """
     black = sdl2.ext.Color(0, 0, 0)                 # Black
     grass = sdl2.ext.Color(76, 175, 80)             # Green 500
-    powerup_power = sdl2.ext.Color(255, 197, 3)     # Amber 500
+    powerup_power = sdl2.ext.Color(255, 152, 0)     # Orange 500
     powerup_bombcount = sdl2.ext.Color(55, 71, 79)  # Blue Grey 800
     powerup_speed = sdl2.ext.Color(3, 169, 244)     # Light Blue 500
     wall_permanent = sdl2.ext.Color(117, 117, 117)  # Grey 600
