8d14cf172fb1dc5ad44a175ba8e273756fcff10d
martonMeszaros <marton.m95@gmail.com>
2017-04-22 12:45:52 +0200

    Proper initial commit

--- common.py
+++
@@ -1,8 +0,0 @@
-import sdl2
-import sdl2.ext
-
-GRASS_COLOR = sdl2.ext.Color(76, 175, 80)
-WALL_PERMANENT = sdl2.ext.Color(117, 117, 117)
-WALL = sdl2.ext.Color(189, 189, 189)
-BLACK = sdl2.ext.Color(0, 0, 0)
-POWERUP = sdl2.ext.Color(3, 169, 244)
